bd2c0a5f7991abcd1b72501537b08a52a2de73ea
[dyad] <manjarrez.84@gmail.com>
2025-11-28 08:17:31 -0600

    [dyad] Reemplazando fundadora.png por fundadora1.png en la página de inicio - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -36,11 +36,11 @@ const Index = () => {
           </div>
         </section>
 
-        {/* Sección con la imagen fundadora.png (sin fondo, marco ni sombra) */}
+        {/* Sección con la imagen fundadora1.png (sin fondo, marco ni sombra) */}
         <section className="py-12 bg-background">
           <div className="container mx-auto px-4 text-center">
             <img 
-              src="/fundadora.png" 
+              src="/fundadora1.png" 
               alt="Imagen de la Fundadora" 
               className="mx-auto w-full max-w-4xl h-auto" 
             />
