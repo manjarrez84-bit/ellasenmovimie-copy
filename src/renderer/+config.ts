c33f41ab3dabc0c38ab6f5f36c220e0c7f3ef11c
[dyad] <manjarrez.84@gmail.com>
2026-09-10 10:13:49 -0600

    [dyad] Corrected Vike React extension package - wrote 4 file(s), added vike-react package(s) + extra files edited outside of Dyad

--- src/renderer/+config.ts
+++ src/renderer/+config.ts
@@ -5,5 +5,5 @@ export default {
   passToClient: ['pageProps', 'routeParams'],
   clientRouting: true,
   hydrationCanBeAborted: true,
-  extends: 'vike-react', // Confirmed to use 'vike-react'
+  extends: 'vike-react', // Corrected to use 'vike-react'
 } satisfies Config;
