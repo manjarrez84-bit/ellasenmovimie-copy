a57af2249dcef4272afa60bf58f45b035b8d345c
[dyad] <manjarrez.84@gmail.com>
2026-09-10 10:21:52 -0600

    Reverted all changes back to version b6e7510d3c010bdd1dec9f2ec7b0265618e390db

--- src/renderer/+config.ts
+++ src/renderer/+config.ts
@@ -1,10 +1,9 @@
 import type { Config } from 'vike/types';
-import vikeReact from 'vike-react/config'; // Import the vike-react config object
 
 // https://vike.dev/config
 export default {
-  extends: vikeReact, // Correctly extend the imported vikeReact config
   passToClient: ['pageProps', 'routeParams'],
   clientRouting: true,
   hydrationCanBeAborted: true,
+  extends: 'vike-react', // Confirmed to use 'vike-react'
 } satisfies Config;
