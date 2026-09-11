d901b6911c8814b4b74d45ef038ee6b78b03e507
[dyad] <manjarrez.84@gmail.com>
2026-09-11 08:49:57 -0600

    [dyad] wrote 6 file(s), added vike-react package(s) + extra files edited outside of Dyad

--- src/pages/[404].tsx
+++ src/pages/[404].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'vike-react/Link';
-import { AttributionFooter } from '@/components/AttributionFooter";
+import { AttributionFooter } from "@/components/AttributionFooter";
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { AttributionFooter } from '@/components/AttributionFooter';
