550b0e15f7b848d451ffc8b1ab88301075c932b7
Dyad <manjarrez.84@gmail.com>
2026-09-12 17:50:47 -0600

    Add 1 file, update 1 file, remove 1 file

--- src/pages/_error/+Page.tsx
+++ src/pages/_error/+Page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
 import { useEffect } from "react";
-import Link from '@/components/Link';
 import { AttributionFooter } from "@/components/AttributionFooter";
 import { usePageContext } from 'vike-react/usePageContext';
 
 const Page = () => {
   const pageContext = usePageContext();
-  useEffect(() => { console.error("404 Error:", pageContext.urlPathname); }, [pageContext.urlPathname]);
+  useEffect(() => {
+    console.error("Error:", pageContext.urlPathname);
+  }, [pageContext.urlPathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex-grow flex items-center justify-center bg-gray-100">
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">404</h1>
           <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-          <Link to="/">Return to Home</Link>
+          <a href="/">Return to Home</a>
         </div>
       </div>
       <AttributionFooter />
